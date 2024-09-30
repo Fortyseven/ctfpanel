4cf45e6ef359f74b9995eaca907ca25a21ba1d18
Fortyseven <fortyseven@network47.org>
2024-09-30 02:45:53 -0400

    dev: rebuild

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -1,46 +1,46 @@
-'use strict';
+"use strict";
 
 function openPopupImage(url) {
-    var popup = document.getElementById('PopupContainer');
+    var popup = document.getElementById("PopupContainer");
     popup.classList.add("open");
 
-    var content = popup.getElementsByClassName('content')[0];
-    content.innerHTML = "<div class='img' style='background-image:url(" + url + ")'/>";
+    var content = popup.getElementsByClassName("content")[0];
+    content.innerHTML =
+        "<div class='img' style='background-image:url(" + url + ")'/>";
 }
 
 function closePopup() {
-    var popup = document.getElementById('PopupContainer');
+    var popup = document.getElementById("PopupContainer");
     popup.classList.remove("open");
 }
 
-
 // Tabs
 
-$(document).ready(function() {
-    $("button[data-for-tab]").each(function(i, el) {
-        $(el).on('click', function() {
-            changePage($(this).attr('data-for-tab'));
-        })
+$(document).ready(function () {
+    $("button[data-for-tab]").each(function (i, el) {
+        $(el).on("click", function () {
+            changePage($(this).attr("data-for-tab"));
+        });
     });
 
     var hash = window.location.hash;
 
     // remove the hash if we're on the main page
-    if (hash == '#main') {
-        history.replaceState(null, null, ' ');
-        changePage('main', true);
+    if (hash == "#main") {
+        history.replaceState(null, null, " ");
+        changePage("main", true);
     }
     // otherwise change to that page if we have one
     else if (hash) {
         changePage(hash.substr(1));
     } else {
-        changePage('main', true);
+        changePage("main", true);
     }
     //NOTE: Not particularly concerned about invalid pages
 });
 
 function changePage(tab_id, skip_hash = false) {
-    $('tabpage').hide();
+    $("tabpage").hide();
 
     $("#" + tab_id).show();
 
@@ -48,15 +48,15 @@ function changePage(tab_id, skip_hash = false) {
         window.location.hash = tab_id;
     }
 
-    $("tabselector button").removeClass('selected');
-    $("tabselector button[data-for-tab='" + tab_id + "'").addClass('selected');
+    $("tabselector button").removeClass("selected");
+    $("tabselector button[data-for-tab='" + tab_id + "'").addClass("selected");
     window.scrollTo(0, 0);
 }
 
-window.ready = function(fn) {
-    if (document.readyState != 'loading') {
+window.ready = function (fn) {
+    if (document.readyState != "loading") {
         fn();
     } else {
-        document.addEventListener('DOMContentLoaded', fn);
+        document.addEventListener("DOMContentLoaded", fn);
     }
-}
+};
